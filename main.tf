4e65e1e4307621e3bdf91a1b353c42e8007c25e1
Parisa Moosavinezhad <parisam@microsoft.com>
2022-09-09 10:35:55 +0200

    update

--- main.tf
+++ main.tf
@@ -26,28 +26,20 @@ module "resourcegroup" {
 }
 
 module "acr_name" {
-  source             = "git::https://eh4amjsb2v7ke7yzqzkviryninjny3urbbq3pbkor25hhdbo5kea@dev.azure.com/p-moosavinezhad/az-iac/_git/az-naming//ac?ref=main"
+  source             = "git::https://eh4amjsb2v7ke7yzqzkviryninjny3urbbq3pbkor25hhdbo5kea@dev.azure.com/p-moosavinezhad/az-iac/_git/az-naming//acr?ref=main"
   prefix             = var.prefix
   name               = var.name
   stage              = var.stage
   location_shortname = var.location_shortname
 }
 
-# module "acr" {
-#   # https://{PAT}@dev.azure.com/{organization}/{project}/_git/{repo-name}
-#   source   = "git::https://eh4amjsb2v7ke7yzqzkviryninjny3urbbq3pbkor25hhdbo5kea@dev.azure.com/p-moosavinezhad/az-iac/_git/az-acr?ref=main"
-#   location = var.location
-#   name     = module.rg_name.result
-#   tags = {
-#     Service         = "Plat. netexc"
-#     AssetName       = "Asset Name"
-#     AssetID         = "AB00CD"
-#     BusinessUnit    = "Plat. netexc Team"
-#     Confidentiality = "C1"
-#     Integrity       = "I1"
-#     Availability    = "A1"
-#     Criticality     = "Low"
-#     Owner           = "parisamoosavinezhad@hotmail.com"
-#     CostCenter      = ""
-#   }
-# }
+module "acr" {
+  # https://{PAT}@dev.azure.com/{organization}/{project}/_git/{repo-name}
+  source              = "git::https://eh4amjsb2v7ke7yzqzkviryninjny3urbbq3pbkor25hhdbo5kea@dev.azure.com/p-moosavinezhad/az-iac/_git/az-acr?ref=main"
+  resource_group_name = module.resourcegroup.name
+  location            = module.resourcegroup.location
+  name                = module.acr_name.result
+  sku                 = "Premium"
+  admin_enabled       = "true"
+  additional_tags     = {}
+}
