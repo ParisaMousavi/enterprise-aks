3df9294f468d3609bd8955531a2bd99c97c2173d
Parisa Moosavinezhad <parisam@microsoft.com>
2022-09-22 11:08:48 +0200

    update

--- bash-vote.sh
+++ bash-vote.sh
@@ -15,17 +15,10 @@ export KUBECONFIG=~/.kube/config
 
 az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
 
-# az aks get-credentials --resource-group projn-rg-app-dev-weu --name projn-aks-app-dev-weu --overwrite-existing --file ~/.kube/config
 
 echo "az aks get-credentials --overwrite-existing --resource-group $resourcegroupname --name $aksclustername"
 
 az aks get-credentials --overwrite-existing --resource-group "$resourcegroupname" --name "$aksclustername" --file ~/.kube/config
-
-
-
-# echo $ARM_CLIENT_ID
-# echo $ARM_CLIENT_SECRET
-# echo $KUBECONFIG
 
 kubelogin convert-kubeconfig -l spn --client-id $ARM_CLIENT_ID --client-secret $ARM_CLIENT_SECRET
 
