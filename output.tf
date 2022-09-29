8364fc24c97da62c5ac8e4856274f1a4d3e95bdd
Parisa Moosavinezhad <parisam@microsoft.com>
2022-09-29 15:42:27 +0200

    update

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "acr_login_server" {
   value = module.acr.others.login_server
 }
+
+output "aks_cluster_id" {
+  value = module.aks.id
+}
+
+output "aks_cluster_resourcegroup" {
+  value = module.resourcegroup.name
+}
