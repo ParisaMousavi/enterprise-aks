701ab993c3d6ba7e1866e4d217fcd99067f46c77
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2023-03-03 15:20:13 +0100

    update

--- main-github.tf
+++ main-github.tf
@@ -17,3 +17,23 @@
 #   secret_name     = "aks_cluster_resourcegroup_name"
 #   plaintext_value = module.resourcegroup.name
 # }
+
+# Pass the namespace to github to deploy workload identity demo via GitHub action.
+# GitHub Action Name: https://github.com/ParisaMousavi/github-actions/actions/workflows/az-aks-workload-identity-sample.yml
+# resource "github_actions_secret" "Workload_Identity_Sample_Namespace" {
+#   depends_on = [
+#     azurerm_federated_identity_credential.this
+#   ]
+#   repository      = local.repository
+#   secret_name     = "Workload_Identity_Sample_Namespace"
+#   plaintext_value = local.aks_service_account_namespace
+# }
+
+# resource "github_actions_secret" "KEYVAULT_URL" {
+#   depends_on = [
+#     azurerm_federated_identity_credential.this
+#   ]
+#   repository      = local.repository
+#   secret_name     = "KEYVAULT_URL"
+#   plaintext_value = module.keyvault.vault_uri
+# }
--- main-workload-identity.tf
+++ main-workload-identity.tf
@@ -60,23 +60,3 @@ resource "azurerm_federated_identity_credential" "this" {
   parent_id           = module.aks_workload_m_id[0].id
   subject             = "system:serviceaccount:${local.aks_service_account_namespace}:${local.aks_servive_account_name}"
 }
-
-# Pass the namespace to github to deploy workload identity demo via GitHub action.
-# GitHub Action Name: https://github.com/ParisaMousavi/github-actions/actions/workflows/az-aks-workload-identity-sample.yml
-resource "github_actions_secret" "Workload_Identity_Sample_Namespace" {
-  depends_on = [
-    azurerm_federated_identity_credential.this
-  ]
-  repository      = local.repository
-  secret_name     = "Workload_Identity_Sample_Namespace"
-  plaintext_value = local.aks_service_account_namespace
-}
-
-resource "github_actions_secret" "KEYVAULT_URL" {
-  depends_on = [
-    azurerm_federated_identity_credential.this
-  ]
-  repository      = local.repository
-  secret_name     = "KEYVAULT_URL"
-  plaintext_value = module.keyvault.vault_uri
-}
